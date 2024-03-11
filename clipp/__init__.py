97d4a595a37f66834364519c4de0cbabb16c60ae
ben <b.ohling@outlook.com>
2024-03-11 01:13:28 -0700

    refactor: remove NULL from imports and __all__

--- clipp/__init__.py
+++ clipp/__init__.py
@@ -3,9 +3,9 @@ A POSIX-compliant, CLI parser library for building CLI interfaces, designed
 to be flexible, intelligent, and uncompromisingly simple. Clipp aims to make
 code more reusable and easily scalable, without compromising performance.
 """
-from clipp.core import Command, OptionGroup, NULL
+from clipp.core import Command, OptionGroup
 
 __author__ = "Ben Ohling"
 __copyright__ = f"Copyright (C) 2024, {__author__}"
 __version__ = "0.2.0"
-__all__ = ["Command", "OptionGroup", "NULL"]
+__all__ = ["Command", "OptionGroup"]
