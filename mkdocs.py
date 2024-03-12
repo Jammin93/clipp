4e4e0cef205d1dff29b27b43ab98470785310406
ben <b.ohling@outlook.com>
2024-03-12 06:48:24 -0700

    fix: full-rewrite of main

--- mkdocs.py
+++ mkdocs.py
@@ -3,12 +3,37 @@
 
 def main():
     with open("readme-doctest.rst", "r") as f:
-        lines = [ln.replace("\n", "") for ln in f.readlines()]
+        lines = f.readlines()
+        undoctest = False
+        start = "# <undoctest>"
+        end = "# </undoctest>"
         for i, ln in enumerate(lines):
-            lines[i] = ln.replace("# doctest: +SKIP", "").rstrip(" ")
+            ln = ln.replace("# doctest: +SKIP", "").rstrip(" ")
+            if undoctest:
+                ln = ln.replace(">>> ", "")
+                if end in ln:
+                    ln = ln.replace(end, "").rstrip()
+                    if ln.strip() == "":
+                        lines[i] = None
 
+                    undoctest = False
+                else:
+                    lines[i] = ln
+            elif start in ln:
+                ln = ln.replace(">>> ", "")
+                ln = ln.replace(start, "").rstrip()
+                if ln.strip() == "":
+                    lines[i] = None
+                else:
+                    lines[i] = ln
+
+                undoctest = True
+            else:
+                lines[i] = ln
+
+    lines = [ln for ln in lines if ln is not None]
     with open("README.rst", "w") as f:
-        f.write("\n".join(lines))
+        f.write("".join(lines))
 
 
 if __name__ == "__main__":
