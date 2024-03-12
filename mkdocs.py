7aea32527697a0b166b728217fd60b994663d0bf
ben <b.ohling@outlook.com>
2024-03-12 05:43:25 -0700

    chore: add mkdocs.py for building README

---
+++ mkdocs.py
@@ -0,0 +1,12 @@
+def main():
+    with open("readme-doctest.rst", "r") as f:
+        lines = [ln.replace("\n", "") for ln in f.readlines()]
+        for i, ln in enumerate(lines):
+            lines[i] = ln.replace("# doctest: +SKIP", "").rstrip(" ")
+
+    with open("README.rst", "w") as f:
+        f.write("\n".join(lines))
+
+
+if __name__ == "__main__":
+    main()
---
+++ mkdocs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source ../.venv/bin/activate > /dev/null
+
+#INIT_WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+#
+#cd "$INIT_WD"/docsrc || exit
+#
+#make clean
+#make github
+#
+#sphinx-build . ./_build
+
+echo "$(which python)"
