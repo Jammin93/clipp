8fa2f8815edb1661b782509d1a02cbfd5b11e4db
ben <b.ohling@outlook.com>
2024-03-13 15:04:13 -0700

    chore: bumped major version number to reflect beta status

--- clipp/__init__.py
+++ clipp/__init__.py
@@ -7,5 +7,5 @@ from clipp.core import Command, OptionGroup
 
 __author__ = "Ben Ohling"
 __copyright__ = f"Copyright (C) 2024, {__author__}"
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 __all__ = ["Command", "OptionGroup"]
