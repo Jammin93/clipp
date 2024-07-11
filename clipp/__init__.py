767071e10f164f55cbb3fe955e2060b341b7f15f
Ben Ohling <b.ohling@outlook.com>
2024-07-11 10:22:59 -0700

    chore: bump minor version number

--- clipp/__init__.py
+++ clipp/__init__.py
@@ -7,5 +7,5 @@ from clipp.core import Command, fill_paragraph, OptionGroup
 
 __author__ = "Ben Ohling"
 __copyright__ = f"Copyright (C) 2024, {__author__}"
-__version__ = "1.0.7"
+__version__ = "1.1.0"
 __all__ = ["Command", "fill_paragraph", "OptionGroup"]
